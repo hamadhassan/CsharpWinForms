25eca949dc05a4cd51f6ffceb0e794d544a6e189
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-06-12 19:06:26 +0500

    frame work designing set environment

---
+++ Framework/DeveloperFramework/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DeveloperFramework
+{
+    public class GameObject
+    {
+        PictureBox pb;
+        public GameObject(Image img,int left,int top)
+        {
+            pb = new PictureBox();
+            pb.Image = img;
+            pb.Left = left;
+            pb.Top = top; 
+            pb.Width = img.Width;
+            pb.Height = img.Height;
+            pb.BackColor = Color.Transparent;
+        }
+
+        internal PictureBox Pb { get => pb; set => pb = value; }
+        public void update(int gravity)
+        {
+            pb.Top+=gravity;
+        }
+    }
+}
